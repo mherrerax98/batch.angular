549cd2cf2fcf732f7eb3fd80522e4d1ed8600ec9
mherrera <mherrera@activesoft.com.co>
2024-04-19 18:23:42 -0500

    agrgar url de desarrollo y url de produccion

--- packages/angular/src/app/services/url.ts
+++ packages/angular/src/app/services/url.ts
@@ -1,1 +1,8 @@
-export const API_URL =  'https://web.cloudxirux.com:35607/BatchRAPI';
+import { environment } from "src/environments/environment";
+
+let apiUrl = 'https://localhost:7283';
+if(environment.production){
+    apiUrl =  'https://web.cloudxirux.com:35607/BatchRAPI'
+}
+
+export const API_URL = apiUrl;
